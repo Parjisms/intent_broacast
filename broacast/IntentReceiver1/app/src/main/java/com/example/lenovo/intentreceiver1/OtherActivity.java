c3efe651393c6afe0cdb845e874324cd1b8afaa7
Parjims <1332709193@qq.com>
2017-05-02 20:37:03 +0800

    提交的是intent和broacast的课本案例
    Signed-off-by: Parjims <1332709193@qq.com>

---
+++ broacast/IntentReceiver1/app/src/main/java/com/example/lenovo/intentreceiver1/OtherActivity.java
@@ -0,0 +1,23 @@
+package com.example.lenovo.intentreceiver1;
+
+/**
+ * Created by LENOVO on 2017/4/26.
+ */
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class OtherActivity extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //从Intent当中根据key取得value
+        String value = intent.getStringExtra("testIntent");
+        Log.e("IntentReceiver-->Test", value);
+    }
+
+}
